5e46fed0fc5591c98cc414422dee7217bf73912d
Jeroen Claassens <jeroen.claassens@live.nl>
2021-05-22 21:39:54 +0200

    chore: upgrade to Node ESM (#16)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { readFileSync, readdirSync } from 'fs';
-import { join } from 'path';
+import type { RESTPostAPIChannelMessageResult } from 'discord-api-types/v8';
 import { WebhookClient } from 'discord.js';
-import { RESTPostAPIChannelMessageResult } from 'discord-api-types/v6';
+import { readdir, readFile } from 'fs/promises';
+import { URL } from 'url';
 import { promisify } from 'util';
 
 const jumpRegex = /%JUMP_TO_TOP%/gm;
@@ -23,63 +23,66 @@ function resolveIdentifier(channelName: string): string {
 
 const wait = promisify(setTimeout);
 
-async function main(): Promise<void> {
-	const deployChannelString = process.env.DEPLOY_CHANNELS;
-	const channels = deployChannelString
-		?.trim()
-		.split(/ *, */gm)
-		.map((c) => resolveIdentifier(c));
+const deployChannelString = process.env.DEPLOY_CHANNELS;
+const channels = deployChannelString
+	?.trim()
+	.split(/ *, */gm)
+	.map((c) => resolveIdentifier(c));
 
-	if (!channels) {
-		throw new Error(`[MISSING] No deploy channels provided`);
-	}
-
-	const files = readdirSync(join(__dirname, '..', 'resources'));
-	const missingHooks = channels.filter((c) => !Boolean(process.env[c]));
-	const missingFiles = channels.filter((c) => !files.includes(`${c}.md`));
-
-	if (missingHooks.length) {
-		throw new Error(`[MISSING] No webhook environment variable(s) for ${missingHooks.join(', ')}`);
-	}
-
-	if (missingFiles.length) {
-		throw new Error(`[MISSING] No file for ${missingFiles.map((c) => `${c}.md`).join(', ')}`);
-	}
-
-	for (const channel of channels) {
-		console.log(`[STARTING] Deploying ${channel}`);
-
-		const [hookID, hookToken] = process.env[channel]!.split('/').slice(-2);
-		const hook = new WebhookClient(hookID, hookToken);
-		const fileName = `${channel}.md`;
-
-		const raw = readFileSync(join(__dirname, '..', 'resources', fileName)).toString();
-		const r1 = raw.replace(linkEscapeRegex, linkEscapeReplacer).replace(/"/g, '\\"');
-		const r2 = Object.entries(replacePatterns).reduce((acc, [k, v]) => {
-			const regex = new RegExp(k, 'gm');
-			return acc.replace(regex, v);
-		}, r1);
-		const parts = r2.split('\n\n');
-
-		let firstMessage: RESTPostAPIChannelMessageResult | null = null;
-		for (let part of parts) {
-			if (firstMessage) {
-				part = part.replace(
-					jumpRegex,
-					`https://discord.com/channels/222078108977594368/${firstMessage.channel_id}/${firstMessage.id}`,
-				);
-			}
-			// A raw API response is returned here, not a Message object as the typings indicate
-			const response = ((await hook.send(part, {
-				avatarURL: process.env.WEBHOOK_AVATAR,
-				username: process.env.WEBHOOK_NAME,
-			})) as unknown) as RESTPostAPIChannelMessageResult;
-			if (!firstMessage) firstMessage = response;
-
-			await wait(1000);
-		}
-		hook.destroy();
-	}
+if (!channels) {
+	throw new Error(`[MISSING] No deploy channels provided`);
 }
 
-void main();
+const resourcesDir = new URL('../resources/', import.meta.url);
+
+const files = await readdir(resourcesDir);
+
+const missingHooks = channels.filter((c) => !Boolean(process.env[c]));
+const missingFiles = channels.filter((c) => !files.includes(`${c}.md`));
+
+if (missingHooks.length) {
+	throw new Error(`[MISSING] No webhook environment variable(s) for ${missingHooks.join(', ')}`);
+}
+
+if (missingFiles.length) {
+	throw new Error(`[MISSING] No file for ${missingFiles.map((c) => `${c}.md`).join(', ')}`);
+}
+
+for (const channel of channels) {
+	console.log(`[STARTING] Deploying ${channel}`);
+
+	const [hookID, hookToken] = process.env[channel]!.split('/').slice(-2);
+	const hook = new WebhookClient(hookID, hookToken);
+	const fileName = `${channel}.md`;
+
+	const raw = await readFile(new URL(fileName, resourcesDir), { encoding: 'utf8' });
+	const r1 = raw.replace(linkEscapeRegex, linkEscapeReplacer).replace(/"/g, '\\"');
+	const r2 = Object.entries(replacePatterns).reduce((acc, [k, v]) => {
+		const regex = new RegExp(k, 'gm');
+		return acc.replace(regex, v);
+	}, r1);
+	const parts = r2.split('\n\n');
+
+	let firstMessage: RESTPostAPIChannelMessageResult | null = null;
+	for (let part of parts) {
+		if (firstMessage) {
+			part = part.replace(
+				jumpRegex,
+				`https://discord.com/channels/222078108977594368/${firstMessage.channel_id}/${firstMessage.id}`,
+			);
+		}
+		// A raw API response is returned here, not a Message object as the typings indicate
+		const response = (await hook.send(part, {
+			avatarURL: process.env.WEBHOOK_AVATAR,
+			username: process.env.WEBHOOK_NAME,
+			allowedMentions: {
+				users: [],
+				roles: [],
+			},
+		})) as unknown as RESTPostAPIChannelMessageResult;
+		if (!firstMessage) firstMessage = response;
+
+		await wait(1000);
+	}
+	hook.destroy();
+}
