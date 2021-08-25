8a458488adcb90a107fcd2bd6eb2fddc6d66aa1b
Sugden <28943913+NotSugden@users.noreply.github.com>
2021-08-25 23:53:14 +0100

    chore: update dependencies (#31)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import type { RESTPostAPIChannelMessageResult } from 'discord-api-types/v8';
-import { WebhookClient } from 'discord.js';
+import { APIMessage } from 'discord-api-types/v9';
+import { WebhookClient, Formatters } from 'discord.js';
 import { readdir, readFile } from 'fs/promises';
 import { URL } from 'url';
 import { promisify } from 'util';
@@ -7,9 +7,8 @@ import { promisify } from 'util';
 const jumpRegex = /%JUMP_TO_TOP%/gm;
 
 const linkEscapeRegex = /\[(.+?)\]\((.+?)\)/gm;
-const linkEscapeReplacer = (_: any, p1: string, p2: string): string => {
-	return `[${p1}](<${p2}>)`;
-};
+const linkEscapeReplacer = (_: any, p1: string, p2: string): string =>
+	Formatters.hyperlink(p1, Formatters.hideLinkEmbed(p2));
 
 const replacePatterns = {
 	'%RULES_CHANNEL%': '<#222109930545610754>',
@@ -17,9 +16,7 @@ const replacePatterns = {
 	'%USEFUL_SERVERS_CHANNEL%': '<#237743386864517122>',
 } as const;
 
-function resolveIdentifier(channelName: string): string {
-	return channelName.toUpperCase().replace(/-/gm, '_');
-}
+const resolveIdentifier = (channelName: string): string => channelName.toUpperCase().replace(/-/gm, '_');
 
 const wait = promisify(setTimeout);
 
@@ -51,19 +48,15 @@ if (missingFiles.length) {
 for (const channel of channels) {
 	console.log(`[STARTING] Deploying ${channel}`);
 
-	const [hookID, hookToken] = process.env[channel]!.split('/').slice(-2);
-	const hook = new WebhookClient(hookID, hookToken);
+	const hook = new WebhookClient({ url: process.env[channel]! });
 	const fileName = `${channel}.md`;
 
 	const raw = await readFile(new URL(fileName, resourcesDir), { encoding: 'utf8' });
 	const r1 = raw.replace(linkEscapeRegex, linkEscapeReplacer).replace(/"/g, '\\"');
-	const r2 = Object.entries(replacePatterns).reduce((acc, [k, v]) => {
-		const regex = new RegExp(k, 'gm');
-		return acc.replace(regex, v);
-	}, r1);
+	const r2 = Object.entries(replacePatterns).reduce((acc, [k, v]) => acc.replace(new RegExp(k, 'gm'), v), r1);
 	const parts = r2.split('\n\n');
 
-	let firstMessage: RESTPostAPIChannelMessageResult | null = null;
+	let firstMessage: APIMessage | null = null;
 	for (let part of parts) {
 		if (firstMessage) {
 			part = part.replace(
@@ -72,15 +65,16 @@ for (const channel of channels) {
 			);
 		}
 		// A raw API response is returned here, not a Message object as the typings indicate
-		const response = (await hook.send(part, {
+		const response = await hook.send({
 			avatarURL: process.env.WEBHOOK_AVATAR,
+			content: part,
 			username: process.env.WEBHOOK_NAME,
 			allowedMentions: {
 				users: [],
 				roles: [],
 			},
-		})) as unknown as RESTPostAPIChannelMessageResult;
-		if (!firstMessage) firstMessage = response;
+		});
+		firstMessage ??= response;
 
 		await wait(1000);
 	}
