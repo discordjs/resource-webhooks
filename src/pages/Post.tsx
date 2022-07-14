3ce56027663aa9352d0d9840fece42ff4cf14971
Jeroen Claassens <support@favware.tech>
2022-07-14 07:57:29 +0200

    chore: shit ===> gold
    permalink: http://whatthecommit.com/8d6e44c138296a3bef2399528261ce7b

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,7 +1,12 @@
 import type { FC } from 'react';
 
 const Post: FC<unknown> = () => {
-	return <div>Post</div>;
+	return (
+		<>
+			<p>This page will have forms for sending webhook messages</p>
+			<p>This will have a send button</p>
+		</>
+	);
 };
 
 export default Post;
--- src/pages/Update.tsx
+++ src/pages/Update.tsx
@@ -1,5 +1,12 @@
 const Update = () => {
-	return <p>This page will have forms for updating an existing webhook messages</p>;
+	return (
+		<>
+			<p>This page will have forms for updating an existing webhook messages</p>
+			<p>This will have an update button</p>
+			<p>This will have a "get raw content" button to get the content for a given messageID and Webhook URL</p>
+			<p>That content will be dumped in an input box after which it can be modified and posted</p>
+		</>
+	);
 };
 
 export default Update;
