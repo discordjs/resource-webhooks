a1f735b08fb31a56735adf43ddee9be0ce24df13
Jeroen Claassens <support@favware.tech>
2022-07-17 03:05:42 +0200

    refactor: use svg vercel image from public

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,17 @@
 import { Box, Paper } from '@mui/material';
-import PoweredByVercel from './PoweredByVercel';
 
-const Footer = () => {
-	return (
-		<Paper square sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, maxHeight: 64, minHeight: 64 }} elevation={3}>
-			<Box
-				sx={{
-					display: 'grid',
-					placeContent: 'center',
-					height: 64
-				}}
-			>
-				<PoweredByVercel />
-			</Box>
-		</Paper>
-	);
-};
+const Footer = () => (
+	<Paper square sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, maxHeight: 64, minHeight: 64 }} elevation={3}>
+		<Box
+			sx={{
+				display: 'grid',
+				placeContent: 'center',
+				height: 64
+			}}
+		>
+			<img src="/img/powered-by-vercel.svg" alt="Powered by Vercel" />
+		</Box>
+	</Paper>
+);
 
 export default Footer;
