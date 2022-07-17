7aa6ea4c65c93a2220217eeb9cf8508749c34540
Jeroen Claassens <support@favware.tech>
2022-07-17 23:54:36 +0200

    build: fix import of notistack

--- src/components/Snackbars/SnackbarDismissButton.tsx
+++ src/components/Snackbars/SnackbarDismissButton.tsx
@@ -1,16 +1,20 @@
-import { Close as CloseIcon } from '@mui/icons-material';
+import { Close } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import type { SnackbarKey } from 'notistack';
+import { useSnackbar, type SnackbarKey } from 'notistack';
 import type { FC } from 'react';
 
 interface SnackbarDismissButtonProps {
 	snackbarKey: SnackbarKey;
 }
 
-const SnackbarDismissButton: FC<SnackbarDismissButtonProps> = ({ snackbarKey }) => (
-	<IconButton aria-label="close" color="inherit" sx={{ p: 0.5 }} onClick={() => closeSnackbar(snackbarKey)}>
-		<CloseIcon />
-	</IconButton>
-);
+const SnackbarDismissButton: FC<SnackbarDismissButtonProps> = ({ snackbarKey }) => {
+	const { closeSnackbar } = useSnackbar();
+
+	return (
+		<IconButton aria-label="close" color="inherit" sx={{ p: 0.5 }} onClick={() => closeSnackbar(snackbarKey)}>
+			<Close />
+		</IconButton>
+	);
+};
 
 export default SnackbarDismissButton;
