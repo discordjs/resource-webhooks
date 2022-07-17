5a36c916c304537dfff6da05b3cbc735f7ebb586
Jeroen Claassens <support@favware.tech>
2022-07-17 23:59:25 +0200

    build: create smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,20 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes('node_modules')) {
+						if (id.includes('@mui')) {
+							return 'vendor_mui';
+						}
+
+						return 'vendor'; // all other package goes here
+					}
+				}
+			}
+		}
+	}
 });
