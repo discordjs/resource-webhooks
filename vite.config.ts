1348070bfc0f5bca4e687ac12752ec1308dacbfd
Jeroen Claassens <support@favware.tech>
2022-07-18 22:39:44 +0200

    chore: i cannot believe that it took this long to write a test for this.
    permalink: http://whatthecommit.com/bc747db1406f0f52b7f49228a456e0e1

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,5 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: (id) => {
-					if (id.includes('node_modules')) {
-						if (id.includes('@mui')) {
-							return 'vendor_mui';
-						}
-
-						return 'vendor'; // all other package goes here
-					}
-				}
-			}
-		}
-	}
+	plugins: [react()]
 });
