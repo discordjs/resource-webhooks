27875f113f63fd76c24560243944d6cff7b51ccb
Jeroen Claassens <support@favware.tech>
2022-07-20 08:40:16 +0200

    feat: add roles config button to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,15 +11,16 @@ interface CardData {
 const cardData: CardData[] = [
 	{ primary: 'Post', secondary: 'Go here to create new Webhook posts', to: '/post', toLabel: 'Create Post' },
 	{ primary: 'Update', secondary: 'Go here to update existing Webhook posts', to: '/update', toLabel: 'Update Post' },
-	{ primary: 'Config', secondary: 'Go here to configure your environment', to: '/config/webhooks', toLabel: 'Configuration' }
+	{ primary: 'Configure Webhooks', secondary: 'Go here to configure your stored webhooks', to: '/config/webhooks', toLabel: 'Configure Webhooks' },
+	{ primary: 'Configure Roles', secondary: 'Go here to configure your stored roles', to: '/config/roles', toLabel: 'Configure Roles' }
 ];
 
 const Home = () => {
 	return (
 		<Grid container justifyContent="center" alignContent="center" alignItems="center" spacing={4}>
 			{cardData.map((card, index) => (
-				<Grid item xs={12} sm={6} md={4} key={index}>
-					<Card sx={{ maxWidth: 345, minHeight: 150, maxHeight: 150 }}>
+				<Grid item xs={12} sm={6} md={6} key={index}>
+					<Card sx={{ width: '100%', minHeight: 150, maxHeight: 150 }}>
 						<CardContent>
 							<Typography gutterBottom variant="h5" component="div">
 								{card.primary}
