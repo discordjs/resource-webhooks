84ba63b6b6b1976e9e760b956368beeec42dd836
Jeroen Claassens <support@favware.tech>
2022-12-27 22:04:55 +0100

    chore: stub out id page

--- pages/[...id].vue
+++ pages/[...id].vue
@@ -2,10 +2,10 @@
 	<section class="body-font">
 		<div class="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
 			<div class="lg:flex-grow md:w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100">About me</h1>
+				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100">Header or something</h1>
 
 				<p class="mb-8 leading-relaxed">
-					Greetings, My name is Jeroen Claassens (a.k.a. <span title="My online alias!">Favna</span>) and I manage
+					TODO
 				</p>
 			</div>
 		</div>
