d7cab468e58681b12ec60690b0f719f602e0cc2d
Jeroen Claassens <support@favware.tech>
2022-12-27 22:03:11 +0100

    feat: rewrite footer

--- components/sections/app-footer.vue
+++ components/sections/app-footer.vue
@@ -1,10 +1,19 @@
 <template>
 	<footer class="body-font">
 		<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-			<p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4">
-				© 2019 Jeroen Claassens —
-				<external-link href="https://redirect.favware.tech/twitter" text="@favna_" />
-			</p>
+			<div class="flex flex-wrap flex-row justify-center content-start items-center w-full">
+				<p class="text-sm text-center max-w-full flex-grow-0 basis-full opacity-70 sm:py-2 sm:mt-0 mt-4">
+					Copyright © 2022 The Sapphire Community and its contributors
+				</p>
+
+				<sponsor-link href="https://vercel.com/?utm_source=sapphiredev&utm_campaign=oss" class="mr-2">
+					<nuxt-img src="/images/powered-by-vercel.svg" style="height: 40px" />
+				</sponsor-link>
+
+				<sponsor-link href="https://polypane.app/">
+					<nuxt-img src="/images/powered-by-polypane.svg" style="height: 40px" />
+				</sponsor-link>
+			</div>
 		</div>
 	</footer>
 </template>
