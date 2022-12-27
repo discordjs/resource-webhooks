06ae61977f4f44d48426a8c5e75c09b39f1ecdee
Jeroen Claassens <support@favware.tech>
2022-12-27 23:53:17 +0100

    feat: add home cards

---
+++ components/home-card.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="card w-full bg-base-100 shadow-xl px-2">
+		<div class="card-body">
+			<h2 class="card-title">{{ title }}</h2>
+			<p>{{ description }}</p>
+			<div class="card-actions justify-end">
+				<nuxt-link :to="href">
+					<button class="btn btn-primary normal-case min-w-[10rem]">{{ btnText }}</button>
+				</nuxt-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string; description: string; btnText: string; href: string }>();
+</script>
--- pages/[...id].vue
+++ pages/[...id].vue
@@ -1,11 +1,10 @@
 <template>
 	<section class="body-font">
-		<div class="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
-			<div class="lg:flex-grow md:w-full lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-				<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-100">Header or something</h1>
-
-				<p class="mb-8 leading-relaxed">TODOtest</p>
-			</div>
+		<div class="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full mt-5 px-5">
+			<home-card href="/post" title="Post" description="Go here to create new Webhook posts" btnText="Create Post" />
+			<home-card href="/update" title="Update" description="Go here to update existing Webhook posts" btnText="Update Post" />
+			<home-card href="/configure/webhooks" title="Configure Webhooks" description="Go here to configure your stored webhooks" btnText="Configure Webhooks" />
+			<home-card href="/configure/roles" title="Configure Roles" description="Go here to configure your stored roles" btnText="Configure Roles" />
 		</div>
 	</section>
 </template>
