c124bd298e9670c4708a445ca7e4f2bb129d34c7
Jeroen Claassens <support@favware.tech>
2023-03-11 22:01:16 +0100

    fix: make webhook url non-nullable

--- lib/schemas/postSchema.ts
+++ lib/schemas/postSchema.ts
@@ -2,7 +2,7 @@ import { object, string } from 'yup';
 import { persistedStorageEntrySchema } from '~~/lib/schemas/persistedStorageEntrySchema';
 
 export const postSchema = object({
-	webhookUrl: persistedStorageEntrySchema.required('You have to choose the URL of the webhook to post with.').nullable(),
+	webhookUrl: persistedStorageEntrySchema.required('You have to choose the URL of the webhook to post with.'),
 	text: string().required('The text to post with the webhook is required.'),
 	role: persistedStorageEntrySchema.nullable()
 });
